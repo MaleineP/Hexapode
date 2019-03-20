0581388aefcc1112e7d3b51c06e806327e2c1466
SebastienRico <rico.2.sebastien@gmail.com>
2019-03-20 14:05:37 +0100

    Refactoring code

--- modulePC/ConnexionToPI.py
+++ modulePC/ConnexionToPI.py
@@ -1,24 +1,19 @@
 import socket
 
-login = "serge"
-password = "delacompta"
-
-hote = "192.168.1.19"
+hote = "192.168.1.30"
 port = 12800
 
-connexion_avec_serveur = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-connexion_avec_serveur.connect((hote, port))
-print("Connexion établie avec le serveur sur le port {}".format(port))
+connexion_to_pi = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+connexion_to_pi.connect((hote, port))
+print("Connexion established with the serveur on the port {}".format(port))
 
-msg_a_envoyer = b""
-while msg_a_envoyer != b"fin":
-    msg_a_envoyer = input("> ")
-    # Peut planter si vous tapez des caractères spéciaux
-    msg_a_envoyer = msg_a_envoyer.encode()
-    # On envoie le message
-    connexion_avec_serveur.send(msg_a_envoyer)
-    msg_recu = connexion_avec_serveur.recv(1024)
-    print(msg_recu.decode())  # Là encore, peut planter s'il y a des accents
+msg_to_send = b""
+while msg_to_send != b"fin":
+    msg_to_send = input("> ")
+    msg_to_send = msg_to_send.encode()
+    connexion_to_pi.send(msg_to_send)
+    msg_recived = connexion_to_pi.recv(1024)
+    print(msg_recived.decode())  # Là encore, peut planter s'il y a des accents
 
 print("Fermeture de la connexion")
-connexion_avec_serveur.close()
+connexion_to_pi.close()
